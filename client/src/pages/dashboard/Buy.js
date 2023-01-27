f2f61bc30e808116626693292c588f4708aed2dc
Debaditya <ddev2636@gmail.com>
2023-01-27 19:08:01 +0530

    filter options added

--- client/src/pages/dashboard/Buy.js
+++ client/src/pages/dashboard/Buy.js
@@ -6,6 +6,8 @@ import "../../buy.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { BsWhatsapp, BsCartPlus } from "react-icons/bs";
 import preloader from "../../assets/preloader.gif";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 const Buy = () => {
   const [showModel, setShowModel] = useState(false);
@@ -36,130 +38,293 @@ const Buy = () => {
   const toggleChat = () => {
     setShowChat(!showChat);
   };
+  const [categories, setCategories] = useState("All");
 
   return (
     <>
       {/* <button onClick={buyItem}>Click</button> */}
+      <Stack
+        spacing={2}
+        direction="row"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        {/* <Button variant="contained" onClick={()=>setCategories('')}>All</Button> */}
+        <Button variant="contained" onClick={() => setCategories("All")}>
+          All
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => setCategories("Electronics")}
+        >
+          Electronics
+        </Button>
+        <Button variant="contained" onClick={() => setCategories("Books")}>
+          Books
+        </Button>
+        <Button variant="contained" onClick={() => setCategories("Utensils")}>
+          Utensils
+        </Button>
+        <Button variant="contained" onClick={() => setCategories("Cycles")}>
+          Cycles
+        </Button>
+        <Button variant="contained" onClick={() => setCategories("Others")}>
+          Others
+        </Button>
+      </Stack>
+
       {item ? (
         <div className="main-container">
-          {item.things.map((items) => {
-            return (
-              <>
-                <div
-                  className="box-container"
-                  key={items._id}
-                  onClick={() => handleClick(items)}
-                >
-                  <div className="img-div">
-                    <img
-                      src={`/uploads/${items.image}`}
-                      alt="hello"
-                      className="buy-img"
-                    />
+          {categories === "All" &&
+            item.things.map((items) => {
+              return (
+                <>
+                  <div
+                    className="box-container"
+                    key={items._id}
+                    onClick={() => handleClick(items)}
+                  >
+                    <div className="img-div">
+                      <img
+                        src={`/uploads/${items.image}`}
+                        alt="hello"
+                        className="buy-img"
+                      />
+                    </div>
+                    <div className="desc-div">
+                      <p className="item-name">{items.name}</p>
+                      <button className="cart-btn">
+                        <BsCartPlus />
+                      </button>
+                    </div>
+                    <p className="item-price">{items.price}</p>
+                    <div className="last-div">
+                      <p>{items.desc}</p>
+                      <p>{items.category}</p>
+                      <p>{items.contact}</p>
+                    </div>
                   </div>
-                  <div className="desc-div">
-                    <p className="item-name">{items.name}</p>
-                    <button className="cart-btn">
-                      <BsCartPlus />
-                    </button>
-                  </div>
-                  <p className="item-price">{items.price}</p>
-                  <div className="last-div">
-                    <p>{items.desc}</p>
-                    <p>{items.category}</p>
-                    <p>{items.contact}</p>
-                  </div>
-                </div>
 
-                <div className={showModel ? "model show-model" : "model"}>
-                  <div className="content1">
-                    <button onClick={closeModal} className="close-modal-btn">
-                      <AiOutlineCloseCircle className="internal" />
-                    </button>
-                    {modalData ? (
-                      <div className="modal-container">
-                        <div className="modal-item1">
-                          <img
-                            src={`/uploads/${modalData.image}`}
-                            alt=""
-                            className="modal-img"
-                          />
-                        </div>
-                        <div className="modal-item2">
-                          <h4>{modalData.name}</h4>
-                          <h4>
-                            Price:{" "}
-                            <span className="item-price2">
-                              {modalData.price}
-                            </span>
-                          </h4>
-                          <h4>
-                            Description:{" "}
-                            <span className="item-desc">{modalData.desc}</span>
-                          </h4>
-                          {/* <p>{modalData.category}</p> */}
-                          <h4>
-                            Category:{" "}
-                            <span className="item-contact">
-                              {modalData.category}
-                            </span>
-                          </h4>
-                          <h4>
-                            Contact:{" "}
-                            <span className="item-contact">
-                              {modalData.contact}
-                            </span>
-                          </h4>
+                  <div className={showModel ? "model show-model" : "model"}>
+                    <div className="content1">
+                      <button onClick={closeModal} className="close-modal-btn">
+                        <AiOutlineCloseCircle className="internal" />
+                      </button>
+                      {modalData ? (
+                        <div className="modal-container">
+                          <div className="modal-item1">
+                            <img
+                              src={`/uploads/${modalData.image}`}
+                              alt=""
+                              className="modal-img"
+                            />
+                          </div>
+                          <div className="modal-item2">
+                            <h4>{modalData.name}</h4>
+                            <h4>
+                              Price:{" "}
+                              <span className="item-price2">
+                                {modalData.price}
+                              </span>
+                            </h4>
+                            <h4>
+                              Description:{" "}
+                              <span className="item-desc">
+                                {modalData.desc}
+                              </span>
+                            </h4>
+                            {/* <p>{modalData.category}</p> */}
+                            <h4>
+                              Category:{" "}
+                              <span className="item-contact">
+                                {modalData.category}
+                              </span>
+                            </h4>
+                            <h4>
+                              Contact:{" "}
+                              <span className="item-contact">
+                                {modalData.contact}
+                              </span>
+                            </h4>
 
-                          <h4>
-                            Chat With Seller :{"  "}
-                            <span>
-                              {/* <button className="chat-btn" onClick={chat}>
+                            <h4>
+                              Chat With Seller :{"  "}
+                              <span>
+                                {/* <button className="chat-btn" onClick={chat}>
                                 <BsWhatsapp className="chat-btn2" />
                               </button> */}
-                              <a
-                                href={`https://api.whatsapp.com/send?phone=${modalData.contact}`}
-                                target="_blank"
-                                className="chat-btn"
-                              >
-                                {" "}
-                                <BsWhatsapp className="chat-btn2" />
-                              </a>
-                            </span>
-                          </h4>
-                          <button
-                            className="add-cart"
-                            onClick={() => handleCart(modalData)}
-                          >
-                            Add To Cart <BsCartPlus />
-                          </button>
-
-                          <div>
-                            <button onClick={toggleChat} className="add-cart">
-                              socketIo
+                                <a
+                                  href={`https://api.whatsapp.com/send?phone=${modalData.contact}`}
+                                  target="_blank"
+                                  className="chat-btn"
+                                >
+                                  {" "}
+                                  <BsWhatsapp className="chat-btn2" />
+                                </a>
+                              </span>
+                            </h4>
+                            <button
+                              className="add-cart"
+                              onClick={() => handleCart(modalData)}
+                            >
+                              Add To Cart <BsCartPlus />
                             </button>
-                          </div>
 
-                          <div
-                            className={
-                              showChat ? "model1 show-model1" : "model1"
-                            }
-                          >
-                            <div className="content11">
-                              <button onClick={toggleChat}>close</button>
+                            <div>
+                              <button onClick={toggleChat} className="add-cart">
+                                socketIo
+                              </button>
+                            </div>
+
+                            <div
+                              className={
+                                showChat ? "model1 show-model1" : "model1"
+                              }
+                            >
+                              <div className="content11">
+                                <button onClick={toggleChat}>close</button>
+                              </div>
                             </div>
                           </div>
+                          ;
                         </div>
-                        ;
-                      </div>
-                    ) : (
-                      <div>no data</div>
-                    )}
+                      ) : (
+                        <div>no data</div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </>
-            );
-          })}
+                </>
+              );
+            })}
+
+          {categories !== "All" &&
+            item.things
+              .filter((items) => items.category === categories)
+              .map((items) => {
+                return (
+                  <>
+                    <div
+                      className="box-container"
+                      key={items._id}
+                      onClick={() => handleClick(items)}
+                    >
+                      <div className="img-div">
+                        <img
+                          src={`/uploads/${items.image}`}
+                          alt="hello"
+                          className="buy-img"
+                        />
+                      </div>
+                      <div className="desc-div">
+                        <p className="item-name">{items.name}</p>
+                        <button className="cart-btn">
+                          <BsCartPlus />
+                        </button>
+                      </div>
+                      <p className="item-price">{items.price}</p>
+                      <div className="last-div">
+                        <p>{items.desc}</p>
+                        <p>{items.category}</p>
+                        <p>{items.contact}</p>
+                      </div>
+                    </div>
+
+                    <div className={showModel ? "model show-model" : "model"}>
+                      <div className="content1">
+                        <button
+                          onClick={closeModal}
+                          className="close-modal-btn"
+                        >
+                          <AiOutlineCloseCircle className="internal" />
+                        </button>
+                        {modalData ? (
+                          <div className="modal-container">
+                            <div className="modal-item1">
+                              <img
+                                src={`/uploads/${modalData.image}`}
+                                alt=""
+                                className="modal-img"
+                              />
+                            </div>
+                            <div className="modal-item2">
+                              <h4>{modalData.name}</h4>
+                              <h4>
+                                Price:{" "}
+                                <span className="item-price2">
+                                  {modalData.price}
+                                </span>
+                              </h4>
+                              <h4>
+                                Description:{" "}
+                                <span className="item-desc">
+                                  {modalData.desc}
+                                </span>
+                              </h4>
+                              {/* <p>{modalData.category}</p> */}
+                              <h4>
+                                Category:{" "}
+                                <span className="item-contact">
+                                  {modalData.category}
+                                </span>
+                              </h4>
+                              <h4>
+                                Contact:{" "}
+                                <span className="item-contact">
+                                  {modalData.contact}
+                                </span>
+                              </h4>
+
+                              <h4>
+                                Chat With Seller :{"  "}
+                                <span>
+                                  {/* <button className="chat-btn" onClick={chat}>
+                                <BsWhatsapp className="chat-btn2" />
+                              </button> */}
+                                  <a
+                                    href={`https://api.whatsapp.com/send?phone=${modalData.contact}`}
+                                    target="_blank"
+                                    className="chat-btn"
+                                  >
+                                    {" "}
+                                    <BsWhatsapp className="chat-btn2" />
+                                  </a>
+                                </span>
+                              </h4>
+                              <button
+                                className="add-cart"
+                                onClick={() => handleCart(modalData)}
+                              >
+                                Add To Cart <BsCartPlus />
+                              </button>
+
+                              <div>
+                                <button
+                                  onClick={toggleChat}
+                                  className="add-cart"
+                                >
+                                  socketIo
+                                </button>
+                              </div>
+
+                              <div
+                                className={
+                                  showChat ? "model1 show-model1" : "model1"
+                                }
+                              >
+                                <div className="content11">
+                                  <button onClick={toggleChat}>close</button>
+                                </div>
+                              </div>
+                            </div>
+                            ;
+                          </div>
+                        ) : (
+                          <div>no data</div>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
         </div>
       ) : (
         <div className="preloader">No Data</div>
